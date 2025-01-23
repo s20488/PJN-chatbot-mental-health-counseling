971278e857ec23f072096387cac8428d4ca733f8
s20488 <s20488@pjwstk.edu.pl>
2025-01-23 11:27:00 +0100

    fix

--- pjn_project/finetuning_model.py
+++ pjn_project/finetuning_model.py
@@ -116,6 +116,7 @@ model = AutoModelForCausalLM.from_pretrained(
     model_name,
     quantization_config=bnb_config,
     device_map=device_map,
+    torch_dtype=torch.bfloat16,
 )
 
 model.config.use_cache = False
@@ -175,22 +176,8 @@ logging.set_verbosity(logging.CRITICAL)
 
 # Run text generation pipeline with our next model
 prompt = "How can I get to a place where I can be content from day to day?"
-pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
-result = pipe(f"<s>[INST] {prompt} [/INST]")
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200, torch_dtype=torch.bfloat16)
+result = pipe(f"<s>[INST] {prompt} [/INST]", dtype=torch.bfloat16)
 print(result[0]["generated_text"])
 
-# Reload model in FP16 and merge it with LoRA weights
-base_model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    device_map=device_map,
-)
-model = PeftModel.from_pretrained(base_model, new_model)
-model = model.merge_and_unload()
 
-# Reload tokenizer to save it
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
