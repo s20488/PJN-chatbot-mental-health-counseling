be0c255a6c23c4d9adc36263f87b133d9d7b7d73
s20488 <s20488@pjwstk.edu.pl>
2025-01-25 11:45:20 +0100

    fix

--- chatbot_mental_health_counseling/__init__.py
+++ chatbot_mental_health_counseling/__init__.py
@@ -1,3 +1,3 @@
-from chatbot_mental_health_counseling import model_finetuning
-from chatbot_mental_health_counseling import model_run
+from chatbot_mental_health_counseling import finetuning
+from chatbot_mental_health_counseling import evaluation
 from chatbot_mental_health_counseling import metrics
