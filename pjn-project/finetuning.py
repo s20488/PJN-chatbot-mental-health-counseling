d3b0d07a5eb787b547365bad74d24427c0f5b305
s20488 <s20488@pjwstk.edu.pl>
2025-01-30 11:22:21 +0100

    fix

--- pjn-project/finetuning.py
+++ pjn-project/finetuning.py
@@ -50,7 +50,7 @@ dataset = dataset.map(preprocess_function)
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=getattr(torch, "float16"),
+    bnb_4bit_compute_dtype=getattr(torch, "bfloat16"),
     bnb_4bit_use_double_quant=False,
 )
 
